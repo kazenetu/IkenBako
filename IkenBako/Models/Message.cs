2b221fcb8d1dde013972d2df82ed20ee68576754
kazenetu <rakudafactory@gmail.com>
2019-11-06 20:44:41 +0900

    メッセージクラスを追加

---
+++ IkenBako/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace IkenBako.Models
+{
+  /// <summary>
+  /// 意見メッセージ
+  /// </summary>
+  public class Message
+  {
+    /// <summary>
+    /// メッセージ対象
+    /// </summary>
+    public string SendTo { get; set; } = "";
+
+    /// <summary>
+    /// メッセージ本文
+    /// </summary>
+    public string Detail { get; set; }
+  }
+}
