0711d8d20d47fab9410a711ded4dc6b4ccb048a1
kazenetu <rakudafactory@gmail.com>
2019-11-08 22:30:38 +0900

    意見投稿画面を追加

--- IkenBako/Pages/Index.cshtml.cs
+++ IkenBako/Pages/Index.cshtml.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using IkenBako.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace IkenBako.Pages
 {
   public class IndexModel : PageModel
   {
     private readonly ILogger<IndexModel> _logger;
+
+    public Message Message { get; set; }
+
+    public List<SelectListItem> SendTargetList { 
+      get
+      {
+        var names = SendTargets.GetInstance().Targets.Select(item => item.Name);
+        return names.Select(name => new SelectListItem(name, name,false)).ToList();
+      }
+    }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -19,7 +28,12 @@ namespace IkenBako.Pages
 
     public void OnGet()
     {
-
     }
+
+    public void OnPost(Message message)
+    {
+      // メッセージインスタンスの処理
+    }
+
   }
 }
