cf7cac50f7f54e8dcfa6550a16dd19236913697a
kazenetu <rakudafactory@gmail.com>
2019-11-09 16:19:43 +0900

    送信完了ページ追加

---
+++ IkenBako/Pages/SendSuccess.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+
+namespace IkenBako.Pages
+{
+    public class SendSuccessModel : PageModel
+    {
+    private readonly ILogger<SendSuccessModel> _logger;
+
+    public SendSuccessModel(ILogger<SendSuccessModel> logger)
+    {
+      _logger = logger;
+    }
+
+    public void OnGet()
+        {
+
+        }
+    }
+}
