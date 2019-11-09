cb00327dd1b48ef88963ec7bd18cc0078f8dc4a4
kazenetu <rakudafactory@gmail.com>
2019-11-09 15:06:16 +0900

    意見メッセージ保存用リポジトリを追加

---
+++ IkenBako/Infrastructures/IMessageRepository.cs
@@ -0,0 +1,24 @@
+﻿using IkenBako.Models;
+using System.Collections.Generic;
+namespace IkenBako.Infrastructures
+{
+  /// <summary>
+  /// 意見メッセージリポジトリインターフェース
+  /// </summary>
+  public interface IMessageRepository
+  {
+    /// <summary>
+    /// 保存
+    /// </summary>
+    /// <param name="sessamge">意見メッセージクラス</param>
+    /// <returns>保存結果turns>
+    bool Save(Message sessamge);
+
+    /// <summary>
+    /// 送信対象宛ての意見メッセージ取得
+    /// </summary>
+    /// <param name="sendTarget">送信対象者クラス</param>
+    /// <returns>意見メッセージリスト</returns>
+    List<Message> FindMessage(SendTarget sendTarget);
+  }
+}
---
+++ IkenBako/Infrastructures/MessageRepository.cs
@@ -0,0 +1,32 @@
+﻿using IkenBako.Models;
+using System.Collections.Generic;
+
+namespace IkenBako.Infrastructures
+{
+  /// <summary>
+  /// 意見メッセージリポジトリクラス
+  /// </summary>
+  public class MessageRepository: IMessageRepository
+  {
+    /// <summary>
+    /// 保存
+    /// </summary>
+    /// <param name="sessamge">意見メッセージクラス</param>
+    /// <returns>保存結果turns>
+    public bool Save(Message sessamge)
+    {
+      return false;
+    }
+
+    /// <summary>
+    /// 送信対象宛ての意見メッセージ取得
+    /// </summary>
+    /// <param name="sendTarget">送信対象者クラス</param>
+    /// <returns>意見メッセージリスト</returns>
+    public List<Message> FindMessage(SendTarget sendTarget)
+    {
+      var result = new List<Message>();
+      return result;
+    }
+  }
+}
