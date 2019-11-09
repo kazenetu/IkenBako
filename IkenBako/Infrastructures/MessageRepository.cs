237cf91878b732913657c8aa084346057ef6fe99
kazenetu <rakudafactory@gmail.com>
2019-11-09 17:30:19 +0900

    保存メソッドの実装

--- IkenBako/Infrastructures/MessageRepository.cs
+++ IkenBako/Infrastructures/MessageRepository.cs
@@ -1,5 +1,8 @@
 ﻿using IkenBako.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace IkenBako.Infrastructures
 {
@@ -9,13 +12,70 @@ namespace IkenBako.Infrastructures
   public class MessageRepository: IMessageRepository
   {
     /// <summary>
+    /// 投稿結果ディレクトリ名
+    /// </summary>
+    private const string SendResultDirectoryName = "SendResult";
+
+    /// <summary>
+    /// ファイル作成の起点パス
+    /// </summary>
+    private string CurrentPath;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public MessageRepository()
+    {
+      CurrentPath = Path.Join(Directory.GetCurrentDirectory(), SendResultDirectoryName);
+
+      // 起点パスが存在しない場合はディレクトリを作成する
+      if (!Directory.Exists(CurrentPath))
+      {
+        Directory.CreateDirectory(CurrentPath);
+      }
+    }
+
+    /// <summary>
     /// 保存
     /// </summary>
     /// <param name="message">意見メッセージクラス</param>
     /// <returns>保存結果<returns>
     public bool Save(Message message)
     {
-      return false;
+      // 格納ディレクトリパスを作成
+      var targetPath = Path.Join(CurrentPath, message.SendTo);
+
+      // 格納ディレクトリパスが存在しない場合はディレクトリを作成する
+      if (!Directory.Exists(targetPath))
+      {
+        Directory.CreateDirectory(targetPath);
+      }
+
+      // 意見メッセージクラスインスタンスをjsonに変換
+      var jsonBytes = ConvartJsonBytes();
+
+      // ファイルパスを作成
+      var filePath = Path.Join(targetPath, $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.txt");
+
+      // ファイルを作成
+      using (FileStream stream = new FileStream(filePath, FileMode.Create))
+      {
+        stream.Write(jsonBytes, 0, jsonBytes.Length);
+      }
+
+      return true;
+
+      // Json変換メソッド
+      Byte[] ConvartJsonBytes()
+      {
+        using (MemoryStream ms = new MemoryStream())
+        {
+          var jsonSerializer = new DataContractJsonSerializer(typeof(Message));
+          jsonSerializer.WriteObject(ms, message);
+          return ms.ToArray();
+        }
+      }
+
     }
 
     /// <summary>
