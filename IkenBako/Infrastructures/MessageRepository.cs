baaf049c85145b70ff27767571fbd93b62a25137
kazenetu <rakudafactory@gmail.com>
2019-11-10 21:58:53 +0900

    送信対象宛ての意見メッセージ取得メソッドの処理実装

--- IkenBako/Infrastructures/MessageRepository.cs
+++ IkenBako/Infrastructures/MessageRepository.cs
@@ -83,7 +83,35 @@ namespace IkenBako.Infrastructures
     public List<Message> FindMessage(SendTarget sendTarget)
     {
       var result = new List<Message>();
+
+      // 対象ディレクトリパスを設定
+      var targetPath = Path.Join(CurrentPath, sendTarget.ID);
+
+      // 対象ディレクトリパスの存在確認
+      if (!Directory.Exists(targetPath))
+      {
+        return result;
+      }
+
+      // 対象ディレクトリからJsonファイルを取得する
+      var targetFiles =  Directory.GetFiles(targetPath, "*.txt");
+      foreach(var filePath in targetFiles)
+      {
+        result.Add(ConvertJsonToMessage(filePath));
+      }
+
       return result;
+
+      // JsonファイルをMessageクラスインスタンスに変換する
+      Message ConvertJsonToMessage(string filePath)
+      {
+        using (var stream = new FileStream(filePath, FileMode.Open))
+        {
+          var jsonSerializer = new DataContractJsonSerializer(typeof(Message));
+          return jsonSerializer.ReadObject(stream) as Message;
+        }
+
+      }
     }
   }
 }
