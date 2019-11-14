98f45fe0df886afedf6f47320564bf6b74786387
kazenetu <rakudafactory@gmail.com>
2019-11-15 00:48:45 +0900

    Infrastructuresを追加

---
+++ Infrastructures/MessageRepository.cs
@@ -0,0 +1,128 @@
+﻿using Domain.Application.Models;
+using Domain.Domain.OpinionMessages;
+using Domain.Domain.Receivers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Infrastructures
+{
+  /// <summary>
+  /// 意見メッセージリポジトリ
+  /// </summary>
+  class MessageRepository: IMessageRepository
+  {
+    /// <summary>
+    /// 投稿結果ディレクトリ名
+    /// </summary>
+    private const string SendResultDirectoryName = "SendResult";
+
+    /// <summary>
+    /// ファイル作成の起点パス
+    /// </summary>
+    private string CurrentPath;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public MessageRepository()
+    {
+      CurrentPath = Path.Join(Directory.GetCurrentDirectory(), SendResultDirectoryName);
+
+      // 起点パスが存在しない場合はディレクトリを作成する
+      if (!Directory.Exists(CurrentPath))
+      {
+        Directory.CreateDirectory(CurrentPath);
+      }
+    }
+
+    /// <summary>
+    /// 破棄
+    /// </summary>
+    public void Dispose()
+    {
+    }
+
+    /// <summary>
+    /// 保存
+    /// </summary>
+    /// <param name="message">意見メッセージクラス</param>
+    public void Save(Message message)
+    {
+      // 格納ディレクトリパスを作成
+      var targetPath = Path.Join(CurrentPath, message.SendTo);
+
+      // 格納ディレクトリパスが存在しない場合はディレクトリを作成する
+      if (!Directory.Exists(targetPath))
+      {
+        Directory.CreateDirectory(targetPath);
+      }
+
+      // 意見メッセージクラスインスタンスをjsonに変換
+      var jsonBytes = ConvartJsonBytes();
+
+      // ファイルパスを作成
+      var filePath = Path.Join(targetPath, $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.txt");
+
+      // ファイルを作成
+      using (FileStream stream = new FileStream(filePath, FileMode.Create))
+      {
+        stream.Write(jsonBytes, 0, jsonBytes.Length);
+      }
+
+      // Json変換メソッド
+      Byte[] ConvartJsonBytes()
+      {
+        using (MemoryStream ms = new MemoryStream())
+        {
+          var jsonSerializer = new DataContractJsonSerializer(typeof(Message));
+          jsonSerializer.WriteObject(ms, message);
+          return ms.ToArray();
+        }
+      }
+
+    }
+
+    /// <summary>
+    /// <summary>
+    /// 送信対象宛ての意見メッセージ取得
+    /// </summary>
+    /// <param name="receiverId">送信対象ID</param>
+    /// <returns>意見メッセージリスト</returns>
+    public List<Message> FindMessage(ReceiverId receiverId)
+    {
+      var result = new List<Message>();
+
+      // 対象ディレクトリパスを設定
+      var targetPath = Path.Join(CurrentPath, receiverId.Value);
+
+      // 対象ディレクトリパスの存在確認
+      if (!Directory.Exists(targetPath))
+      {
+        return result;
+      }
+
+      // 対象ディレクトリからJsonファイルを取得する
+      var targetFiles = Directory.GetFiles(targetPath, "*.txt");
+      foreach (var filePath in targetFiles)
+      {
+        result.Add(ConvertJsonToMessage(filePath));
+      }
+
+      return result;
+
+      // JsonファイルをMessageクラスインスタンスに変換する
+      Message ConvertJsonToMessage(string filePath)
+      {
+        using (var stream = new FileStream(filePath, FileMode.Open))
+        {
+          var jsonSerializer = new DataContractJsonSerializer(typeof(MessageModel));
+          var messageModel = jsonSerializer.ReadObject(stream) as MessageModel;
+          return Message.Create(messageModel.SendTo, messageModel.Detail);
+        }
+
+      }
+    }
+  }
+}
---
+++ Infrastructures/ReceiverRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Domain.Receivers;
+using System.Collections.Generic;
+
+namespace Infrastructures
+{
+  /// <summary>
+  /// 送信対象者リポジトリ
+  /// </summary>
+  public class ReceiverRepository
+  {
+    /// <summary>
+    /// 送信対象者リストを取得
+    /// </summary>
+    /// <returns>送信対象者メッセージリスト</returns>
+    public List<Receiver> GetReceivers()
+    {
+      var result = new List<Receiver>();
+      result.Add(Receiver.Create("Aさん", "aa"));
+      result.Add(Receiver.Create("Bさん", "bb"));
+      result.Add(Receiver.Create("Cさん", "cc"));
+
+      return result;
+    }
+  }
+}
