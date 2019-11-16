217699ddaa1cfb5f2b0f75eeb08810b445bcffff
kazenetu <rakudafactory@gmail.com>
2019-11-16 10:42:44 +0900

    ViewModelクラス追加

---
+++ IkenBako/ViewModels/MessageViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace IkenBako.ViewModels
+{
+  /// <summary>
+  /// メッセージ用ViewModel
+  /// </summary>
+  public class MessageViewModel
+  {
+    /// <summary>
+    /// メッセージ対象
+    /// </summary>
+    public string SendTo { get; set; }
+
+    /// <summary>
+    /// メッセージ本文
+    /// </summary>
+    public string Detail { get; set; }
+
+  }
+}
---
+++ IkenBako/ViewModels/SendTargetViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace IkenBako.ViewModels
+{
+  /// <summary>
+  /// 送信対象者
+  /// </summary>
+  public class SendTargetViewModel
+  {
+    /// <summary>
+    /// 送信表示象名称
+    /// </summary>
+    public string DisplayName { get; set; }
+
+    /// <summary>
+    /// 送信対象ID
+    /// </summary>
+    public string ID { get; set; }
+  }
+}
