4ad42551a96c9179ca4eb5fd854dc9bfea5f8fa9
kazenetu <rakudafactory@gmail.com>
2019-11-18 00:40:57 +0900

    Receivers以下の送信者ファイルを参照する機能を実装

--- Infrastructures/ReceiverRepository.cs
+++ Infrastructures/ReceiverRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Domain.Domain.Receivers;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Infrastructures
 {
@@ -9,17 +11,74 @@ namespace Infrastructures
   public class ReceiverRepository: IReceiverRepository
   {
     /// <summary>
+    /// 送信者ディレクトリ名
+    /// </summary>
+    private const string ReceiverDirectoryName = "Receivers";
+
+    /// <summary>
+    /// ファイルの起点パス
+    /// </summary>
+    private string CurrentPath;
+
+    /// <summary>
+    /// 対象ファイル名
+    /// </summary>
+    private List<string> files = new List<string>();
+
+    /// <summary>
+    /// 送信者リスト
+    /// </summary>
+    private List<Receiver> receivers = new List<Receiver>();
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public ReceiverRepository()
+    {
+      CurrentPath = Path.Join(Directory.GetCurrentDirectory(), ReceiverDirectoryName);
+
+      CheckAndLoadFiles();
+    }
+
+    /// <summary>
     /// 送信対象者リストを取得
     /// </summary>
     /// <returns>送信対象者メッセージリスト</returns>
     public List<Receiver> GetReceivers()
     {
-      var result = new List<Receiver>();
-      result.Add(Receiver.Create("Aさん", "aa"));
-      result.Add(Receiver.Create("Bさん", "bb"));
-      result.Add(Receiver.Create("Cさん", "cc"));
+      CheckAndLoadFiles();
+      return receivers;
+    }
 
-      return result;
+
+    /// <summary>
+    /// 送信対象者ディレクトリのチェックと読み出し
+    /// </summary>
+    private void CheckAndLoadFiles()
+    {
+      // 対象ファイルを取得
+      var targetFiles = Directory.GetFiles(CurrentPath, "*.txt");
+      foreach (var filePath in targetFiles)
+      {
+        // 既存確認
+        if (files.Contains(filePath))
+        {
+          continue;
+        }
+
+        // ファイルの存在確認
+        if (!File.Exists(filePath))
+        {
+          throw new Exception($"送信対象者情報「{filePath}」が存在しません。");
+        }
+
+        var receiverId = (new FileInfo(filePath)).Name.Replace(".txt", string.Empty);
+        var displayName = File.ReadAllText(filePath);
+        receivers.Add(Receiver.Create(displayName, receiverId));
+
+        files.Add(filePath);
+      }
+
     }
   }
 }
