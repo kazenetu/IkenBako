fb79814d4cef26e9c5c973fb704b16a116b45dfb
kazenetu <rakudafactory@gmail.com>
2019-12-06 00:51:05 +0900

    意見メッセージ一覧表示取得用のModelクラス追加

---
+++ Domain/Application/Models/DisplayMessageModel.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Domain.OpinionMessages;
+
+namespace Domain.Application.Models
+{
+  /// <summary>
+  /// 表示用意見メッセージ
+  /// </summary>
+  public class DisplayMessageModel : MessageModel
+  {
+    /// <summary>
+    /// 送信対象者名称
+    /// </summary>
+    public string SendTargetName { get; private set; }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="source">意見メッセージインスタンス</param>
+    /// <param name="sendTargetName">送信対象者名称</param>
+    public DisplayMessageModel(Message source, string sendTargetName) : base(source)
+    {
+      SendTargetName = sendTargetName;
+    }
+  }
+}
