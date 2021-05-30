64e2ebf7b2c7ae090a32427e49801500e9f16e56
kazenetu <rakudafactory@gmail.com>
2021-05-30 22:38:18 +0900

    上司ごとにログインできるようパスワードとソルトを追加 #3

--- docker_dev/docker-entrypoint-initdb.d/init.sh
+++ docker_dev/docker-entrypoint-initdb.d/init.sh
@@ -7,6 +7,8 @@ DROP TABLE IF EXISTS t_message;
 CREATE TABLE m_receiver(
  unique_name varchar(255) NOT NULL,
  fullname varchar(255) NOT NULL,
+ password varchar(255) NOT NULL,
+ salt varchar(255) NOT NULL,
  primary key(unique_name)
 );
 
@@ -26,9 +28,13 @@ comment on column t_message.id                  is '連番';
 comment on column t_message.send_to            is '送信対象の上司名';
 comment on column t_message.detail             is '送信メッセージ';
 
-INSERT INTO m_receiver("unique_name", "fullname") VALUES ('aa', 'Aさん');
-INSERT INTO m_receiver("unique_name", "fullname") VALUES ('bb', 'Bさん');
-INSERT INTO m_receiver("unique_name", "fullname") VALUES ('cc', 'Cさん');
+INSERT INTO m_receiver("unique_name", "fullname", "password", "salt") 
+VALUES ('aa', 'Aさん', '09Dg4qWzhtrppNMV6eNbRLb0Cdxy5kH47Qi8O9BzRqU=', 'E6b0z+WEzgNd9mZ6CS5/+w==');
 
+INSERT INTO m_receiver("unique_name", "fullname", "password", "salt") 
+VALUES ('bb', 'Bさん', 'ti+jOc88+oWfm3aIkg9XGq0No6akBADu6S2fwAImVXQ=', '6toFigaiTXNUVHzBDlDSqw==');
+
+INSERT INTO m_receiver("unique_name", "fullname", "password", "salt") 
+VALUES ('cc', 'Cさん', 'q6cQlhHmvFMBcmvnM3ON61a+qLGzCE3dfcFIpCS4nh4=', 'sXoo48uAMbxMJA9HYifoAA==');
 
 EOSQL
