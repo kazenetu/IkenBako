1065a2c7dba52214ea727567d5d3f5523c307c8a
kazenetu <rakudafactory@gmail.com>
2021-06-12 16:56:53 +0900

    ユーザーマスタをDBに追加 #10

--- docker_dev/docker-entrypoint-initdb.d/init.sh
+++ docker_dev/docker-entrypoint-initdb.d/init.sh
@@ -1,9 +1,19 @@
 set -e
 psql testDB -U test test << EOSQL
 
+DROP TABLE IF EXISTS m_user;
 DROP TABLE IF EXISTS m_receiver;
 DROP TABLE IF EXISTS t_message;
 
+-- ユーザーマスタ
+CREATE TABLE m_user(
+ unique_name varchar(255) NOT NULL,
+ password varchar(255) NOT NULL,
+ salt varchar(255) NOT NULL,
+ primary key(unique_name)
+);
+
+-- 上司マスタ
 CREATE TABLE m_receiver(
  unique_name varchar(255) NOT NULL,
  fullname varchar(255) NOT NULL,
@@ -12,12 +22,18 @@ CREATE TABLE m_receiver(
  primary key(unique_name)
 );
 
+-- メッセージテーブル
 CREATE TABLE t_message(
  id SERIAL NOT NULL,
  send_to varchar(255) NOT NULL,
  detail text NOT NULL,
  primary key(id)
 );
+
+comment on column m_user             is 'ユーザーマスタ';
+comment on column m_user.unique_name is 'ユニークな略称';
+comment on column m_user.password    is '暗号化したパスワード';
+comment on column m_user.salt        is '暗号化パラメータ';
 
 comment on column m_receiver             is '上司マスタ';
 comment on column m_receiver.unique_name is 'ユニークな略称';
@@ -26,10 +42,24 @@ comment on column m_receiver.password    is '暗号化したパスワード';
 comment on column m_receiver.salt        is '暗号化パラメータ';
 
 comment on column t_message                    is 'メッセージテーブル';
-comment on column t_message.id                  is '連番';
+comment on column t_message.id                 is '連番';
 comment on column t_message.send_to            is '送信対象の上司略称';
 comment on column t_message.detail             is '送信メッセージ';
 
+-- ユーザーマスタ登録
+INSERT INTO m_user("unique_name", "password", "salt") 
+VALUES ('aa', '09Dg4qWzhtrppNMV6eNbRLb0Cdxy5kH47Qi8O9BzRqU=', 'E6b0z+WEzgNd9mZ6CS5/+w==');
+
+INSERT INTO m_user("unique_name", "password", "salt") 
+VALUES ('bb', 'ti+jOc88+oWfm3aIkg9XGq0No6akBADu6S2fwAImVXQ=', '6toFigaiTXNUVHzBDlDSqw==');
+
+INSERT INTO m_user("unique_name", "password", "salt") 
+VALUES ('cc', 'q6cQlhHmvFMBcmvnM3ON61a+qLGzCE3dfcFIpCS4nh4=', 'sXoo48uAMbxMJA9HYifoAA==');
+
+INSERT INTO m_user("unique_name", "password", "salt") 
+VALUES ('guest', 'XN+YezObIVLTzVT3jBNtrQSfjtaHXL5zWqW/ci50u6g=', '7Hi5ZraAGng7gQRD6r/bqw==');
+
+-- 上司マスタ登録
 INSERT INTO m_receiver("unique_name", "fullname", "password", "salt") 
 VALUES ('aa', 'Aさん', '09Dg4qWzhtrppNMV6eNbRLb0Cdxy5kH47Qi8O9BzRqU=', 'E6b0z+WEzgNd9mZ6CS5/+w==');
 
