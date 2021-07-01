a41739315ddab8ecd45d0a717f85b0826a80459e
kazenetu <rakudafactory@gmail.com>
2021-07-01 23:47:34 +0900

    パスワード単位に「暗号化済パスワードとソルトのテキスト」を作成する機能を追加 #13

--- tools/createPassword/createPassword/Program.cs
+++ tools/createPassword/createPassword/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
@@ -23,18 +24,24 @@ public class Program
       {
         rng.GetBytes(salt);
       }
-      Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+      var saltBase64 = Convert.ToBase64String(salt);
+      Console.WriteLine($"Salt: {saltBase64}");
 
       // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
-      string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+      string encryptedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
           password: password,
           salt: salt,
           prf: KeyDerivationPrf.HMACSHA1,
           iterationCount: 10000,
           numBytesRequested: 256 / 8));
-      Console.WriteLine($"Encrypted Password: {hashed}");
+      Console.WriteLine($"Encrypted Password: {encryptedPassword}");
 
       Console.WriteLine("");
+
+      // ファイル作成
+      Directory.CreateDirectory("output");
+      var filePath = $"output/{password}.txt";
+      File.WriteAllText(filePath,$"> Encrypted Password:{Environment.NewLine}{encryptedPassword}{Environment.NewLine}> Salt:{Environment.NewLine}{saltBase64}"); 
     }
   }
 }
